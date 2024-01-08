2439c9dc750811ca3dbb5a35a9d5af2321fbcdba
Esra Aydemir <es.altintas@gmail.com>
2024-01-08 09:05:37 +0300

    Add count beside cart icon

--- src/app/component/Header/Header.tsx
+++ src/app/component/Header/Header.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import styles from "./header.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import useCart from "@/app/(store)/store";
 
 export function Header() {
+  const { cart } = useCart();
+  console.log(cart);
   return (
     <div className={`${styles.headerWrapper}`} data-testid="header">
       <Link href={"/"}>
@@ -19,6 +24,7 @@ export function Header() {
 
       <h5 className="text-sm">Ceezer Marketplace</h5>
       <div>
+        <span className="text-neutral-600">{cart?.length}</span>
         <Link href="/cart">
           <FontAwesomeIcon icon={faCartShopping} width={20} height={20} />
         </Link>
